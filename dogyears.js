de4481d5ca1038668e18304c1c63c512363c4d53
Marshay McMorris <marshaychianti@yahoo.com>
2021-02-03 12:32:00 -0600

    Initial commit

---
+++ dogyears.js
@@ -0,0 +1,18 @@
+/ Created a variable and stored my age.
+let myAge = 25;
+// Created a variable to represent early years.
+let earlyYears = 2;
+earlyYears *= 10.5;
+//Storing value for the variable laterYears
+let laterYears = myAge - 2;
+//Calculates the number in dog years
+laterYears *= 4;
+//Logs results to console
+console.log(earlyYears);
+console.log(laterYears);
+//Will calculate my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+//Creates variable, stores my name in lowercase.
+let myName = 'Marshay'.toLowerCase();
+//Logs results to console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears}`);
